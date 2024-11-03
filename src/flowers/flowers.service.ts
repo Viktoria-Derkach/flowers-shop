6c887d2c2d01608c9a14ac7e2f4b2793f2d7ffca
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2024-11-03 13:29:09 +0200

    prisma

--- src/flowers/flowers.service.ts
+++ src/flowers/flowers.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class FlowersService {
+  constructor(private readonly prismaService: PrismaService) {}
   findAll() {
-    return [{ name: 'red', color: 'red', price: 1 }];
+    return this.prismaService.flower.findMany();
+    // return [{ name: 'red', color: 'red', price: 1 }];
   }
 }
